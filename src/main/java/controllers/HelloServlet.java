837a845aa89af0f6b8412350c9c38f0e0bd01445
ldanh270 <ducanhle.dn@gmail.com>
2026-02-02 22:16:42 +0700

    refactor: refactor codebase to maven vscode

---
+++ src/main/java/controllers/HelloServlet.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+
+    resp.setContentType("text/html;charset=UTF-8");
+    resp.getWriter().println("<h1>Hello Servlet OK</h1>");
+  }
+}
