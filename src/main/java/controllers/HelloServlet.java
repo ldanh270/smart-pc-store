8adef6c329072124a9e3fe5080b6c1f8b815bf46
ldanh270 <ducanhle.dn@gmail.com>
2026-02-03 14:39:52 +0700

    chore: change project name to smart-pc-store

--- src/main/java/controllers/HelloServlet.java
+++ src/main/java/controllers/HelloServlet.java
@@ -10,8 +10,8 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    resp.setContentType("text/html;charset=UTF-8");
-    resp.getWriter().println("<h1>Hello Servlet OK</h1>");
+  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    res.setContentType("text/html;charset=UTF-8");
+    res.getWriter().println("<h1>Hello Servlet OK</h1>");
   }
 }
