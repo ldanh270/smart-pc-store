3a360aaf3bd653e6bb9483ccef0926298f6d0730
ldanh270 <ducanhle.dn@gmail.com>
2026-02-07 15:27:50 +0700

    feat: setup base structure for auth

--- src/main/java/dao/GenericDAO.java
+++ src/main/java/dao/GenericDAO.java
@@ -1,118 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 
 import java.util.List;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
+public class GenericDAO<T> implements IGenericDAO<T> {
+    private final Class<T> entityClass;
 
-/**
- * Include DAO functions for access to database
- *
- * @author ducan
- */
-public class GenericDAO {
+    protected EntityManager em;
 
-    /**
-     * Find records by IDs
-     *
-     * @param entityClass Proper type
-     * @param id          Object id to find
-     * @return Result object
-     */
-    public <T, ID> T findById(Class<T> entityClass, ID id) {
-        try (EntityManager em = JPAUtil.getEntityManager()) {
-            return em.find(entityClass, id);
-        }
+    public GenericDAO(Class<T> entityClass, EntityManager em) {
+        this.entityClass = entityClass;
+        this.em = em;
     }
 
-    /**
-     * Find all records
-     *
-     * @param entityClass Proper type
-     * @return List of records
-     */
-    public <T> List<T> findAll(Class<T> entityClass) {
-        try (EntityManager em = JPAUtil.getEntityManager()) {
-            String jpql = "SELECT e FROM " + entityClass.getSimpleName() + " e";
-            return em.createQuery(jpql, entityClass).getResultList();
-        }
+    @Override
+    public T findById(Object id) {
+        return em.find(entityClass, id);
     }
 
-    /**
-     * Save entity
-     *
-     * @param entity Entity to save
-     * @return Saved entity
-     */
-    public <T> T save(T entity) {
-        EntityManager em = JPAUtil.getEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        try {
-            tx.begin();
-            em.persist(entity);
-            tx.commit();
-            return entity;
-        } catch (Exception e) {
-            if (tx.isActive()) {
-                tx.rollback();
-            }
-            throw e;
-        } finally {
-            em.close();
-        }
+    @Override
+    public List<T> findAll() {
+        String jpql = "SELECT e FROM " + entityClass.getSimpleName() + " e";
+        TypedQuery<T> query = em.createQuery(jpql, entityClass);
+        return query.getResultList();
     }
 
-    /**
-     * Update entity
-     *
-     * @param entity Entity to update
-     * @return Updated entity
-     */
-    public <T> T update(T entity) {
-        EntityManager em = JPAUtil.getEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        try {
-            tx.begin();
-            T merged = em.merge(entity);
-            tx.commit();
-            return merged;
-        } catch (Exception e) {
-            if (tx.isActive()) {
-                tx.rollback();
-            }
-            throw e;
-        } finally {
-            em.close();
-        }
+    @Override
+    public void create(T entity) {
+        em.persist(entity);
     }
 
-    /**
-     * Delete entity by ID
-     *
-     * @param entityClass Proper type
-     * @param id          Object id to delete
-     */
-    public <T, ID> void deleteById(Class<T> entityClass, ID id) {
-        EntityManager em = JPAUtil.getEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        try {
-            tx.begin();
-            T entity = em.find(entityClass, id);
-            if (entity != null) {
-                em.remove(entity);
-            }
-            tx.commit();
-        } catch (Exception e) {
-            if (tx.isActive()) {
-                tx.rollback();
-            }
-            throw e;
-        } finally {
-            em.close();
+    @Override
+    public T update(T entity) {
+        return em.merge(entity);
+    }
+
+    @Override
+    public void delete(Object id) {
+        T entity = em.find(entityClass, id);
+        if (entity != null) {
+            em.remove(entity);
         }
     }
 }
---
+++ src/main/java/servlets/AuthServlet.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author ducan
+ */
+@WebServlet(name = "AuthServlet", urlPatterns = {"/auth"})
+public class AuthServlet extends HttpServlet {
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+}
