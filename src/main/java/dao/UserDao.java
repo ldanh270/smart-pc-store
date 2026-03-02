67b12cf7f91276f5b14621866db00b85e29a7a32
bloan-7105 <taloan.8104@gmail.com>
2026-03-02 23:03:17 +0700

    fix(user): update UserDao.java

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -37,10 +37,14 @@ public class UserDao extends GenericDao<User> {
      * @return the User entity
      */
     public User findByUsername(String username) {
-        String jpql = "SELECT u FROM User u WHERE u.username = :username";
-        TypedQuery<User> query = em.createQuery(jpql, User.class);
-        query.setParameter("username", username);
-        return query.getSingleResult();
+        try {
+            String jpql = "SELECT u FROM User u WHERE u.username = :username";
+            TypedQuery<User> query = em.createQuery(jpql, User.class);
+            query.setParameter("username", username);
+            return query.getSingleResult();
+        } catch (jakarta.persistence.NoResultException e) {
+            return null;
+        }
     }
 
     /**
