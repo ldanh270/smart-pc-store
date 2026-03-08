e8737af9d7091d10fe385d6cb75830395d56e3a5
ldanh270 <ducanhle.dn@gmail.com>
2026-03-08 15:17:57 +0700

    chore: change CorsFilter to resolve CORS

--- src/main/java/filters/CorsFilter.java
+++ src/main/java/filters/CorsFilter.java
@@ -28,7 +28,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
         res.setHeader("Access-Control-Max-Age", "3600");
